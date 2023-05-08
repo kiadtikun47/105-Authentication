46530427ff0349075692e78f5574381acacea1e6
PpingG_47 <117506450+kiadtikun47@users.noreply.github.com>
2023-05-08 14:40:07 +0700

    new

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const connection = mysql.createConnection({
   host: "server2.bsthun.com",
   port: "6105",
